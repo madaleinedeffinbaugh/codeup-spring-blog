936e5e283fb36647ec12bd1a16e4aa5b27a63a55
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-06-30 10:19:56 -0600

    views lecture

--- src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
@@ -1,15 +1,29 @@
 package com.codeup.codeupspringblog.controllers;
+//package com.codeup.codeupspringblog.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class HomeController {
+//
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String landing(){
+//        return "This is the landing page!";
+//    }
+//}
+
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String landing(){
-        return "This is the landing page!";
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
     }
 }
---
+++ src/main/java/com/codeup/codeupspringblog/models/Item.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupspringblog.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Item {
+    private String name;
+}
